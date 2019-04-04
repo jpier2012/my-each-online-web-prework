343533c0e291d7b640f90f31c2605818930d5adb
James Pier <jpier2012@gmail.com>
2019-04-04 00:46:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
 def my_each array
   i = 0
   while i < array.length
-    yield i
+    yield x
     i += 1
   end
 end
 
 test_array = *(1..10)
 
-puts new_array = my_each(test_array) {|i| i + 10}
+puts new_array = my_each(test_array) {|x| x + 10}
